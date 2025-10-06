1a2d56c7956a21cac62ccddbcb7e343f964bbb8e
Yujie Liu <125097061+YujieLiu666@users.noreply.github.com>
2025-10-06 10:38:38 -0700

    Update function_XGB.py
    update

--- function_XGB.py
+++ function_XGB.py
@@ -383,7 +383,6 @@ def check_model_performance(site_data, predictors, y_col, reg, n_folds=10):
         )
         
         # New style (XGBoost >= 2.0)
-    
         # reg.fit(
         #         X_train, y_train,
         #         eval_set=[(X_train, y_train), (X_test, y_test)],
